a8471a4c6f112369cd353f1844db571f4b0bb733
1453620436 <1453620436@qq.com>
2019-11-27 10:20:01 +0800

    11.27

---
+++ code/MALL/mall-goods/src/main/java/com/mall/goods/MallGoodsApplication.java
@@ -0,0 +1,12 @@
+package com.mall.goods;
+
+
+//@SpringBootApplication
+public class MallGoodsApplication {
+  /* public static void main(String[] args) {
+        SpringApplication.run(MallPaymentApplication.class, args);
+    }*//* public static void main(String[] args) {
+        SpringApplication.run(MallPaymentApplication.class, args);
+    }*/
+
+}
